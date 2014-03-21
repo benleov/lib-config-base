a03b5ccb40d463147e2dc51f8532aca22306598a
Ben Leov <ben@peekwebdesign.com.au>
2014-03-21 12:05:04 +1100

    added extra command to configuration framework

--- src/main/java/lib/config/base/configuration/Configuration.java
+++ src/main/java/lib/config/base/configuration/Configuration.java
@@ -4,20 +4,30 @@ import java.util.Set;
 
 public interface Configuration {
 
-	
 	/**
 	 * 
 	 * @return Returns a unique identifier for this configuration.
 	 * 
 	 */
 	void setId(String id);
-	
+
 	String getId();
 
 	String getProperty(String key);
-	
+
+	/**
+	 * Removes the specified property.
+	 * 
+	 * @param key
+	 *            Key of property to remove.
+	 * 
+	 * @return True if a property was removed. False if no property existed with
+	 *         that key.
+	 */
+	boolean removeProperty(String key);
+
 	void setProperty(String key, String value);
-	
+
 	Set<String> getKeys();
-	
+
 }
--- src/main/java/lib/config/base/configuration/impl/BasicConfiguration.java
+++ src/main/java/lib/config/base/configuration/impl/BasicConfiguration.java
@@ -11,68 +11,71 @@ import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.ElementMap;
 import org.simpleframework.xml.Root;
 
-
 @Root
 public class BasicConfiguration implements Configuration {
 
 	@Attribute
 	private String id;
-	
+
 	@ElementMap(entry = "property", key = "key", attribute = true, inline = true)
 	private Map<String, String> map;
 
 	public BasicConfiguration() {
 		map = new HashMap<String, String>();
 	}
-	
+
 	public void setProperty(String key, String value) {
 		map.put(key, value);
 	}
-	
+
 	public String getProperty(String key) {
 		return map.get(key);
 	}
-	
+
 	public Set<String> getKeys() {
 		return Collections.unmodifiableSet(map.keySet());
 	}
-	
+
+	@Override
+	public boolean removeProperty(String key) {
+		return map.remove(key) != null;
+	}
+
 	public String getProperty(String key, String defaultValue) {
 		String value = map.get(key);
-		
-		if(value == null) {
+
+		if (value == null) {
 			return defaultValue;
 		} else {
 			return value;
 		}
 	}
-	
-	
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		
+
 		builder.append("config[id=" + id);
 		builder.append(" properties=(");
-		
-		for(String key : map.keySet()) {
+
+		for (String key : map.keySet()) {
 			builder.append("[");
 			builder.append(key);
 			builder.append("=");
 			builder.append(map.get(key));
 			builder.append("] ");
 		}
-			
+
 		builder.append(")]");
 		return builder.toString();
 	}
+
 }
- 
